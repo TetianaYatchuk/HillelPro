0d6f5f4fa423f53603ce658ce14ed3b152b3b4de
TetianaYatchuk <tanya15115@gmail.com>
2023-10-11 16:54:21 +0300

    hw2

---
+++ hw2/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let num = prompt('number');
+let num2 = prompt('number2');
+let x = Number(num);
+let y = Number(num2);
+
+console.log('Mult:' + ' ' + num + ' ' + '*' + ' ' + num2 + ' ' + '=' + ' ' + (num * num2));
+
+console.log('Div:' + ' ' + num + ' ' + '/' + ' ' + num2 + ' ' + '=' + ' ' + (num / num2));
+
+console.log('Diff:' + ' ' + num + ' ' + '-' + ' ' + num2 + ' ' + '=' + ' ' + (num - num2));
+
+console.log('Sum:' + ' ' + num + ' ' + '+' + ' ' + num2 + ' ' + '=' + ' ' + (x + y));
+
