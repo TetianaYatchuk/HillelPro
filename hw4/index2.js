780caa7eac3d8bc4ec4e4c89b55f8a7d74dfecf7
TetianaYatchuk <tanya15115@gmail.com>
2023-10-17 17:11:15 +0300

    hw4(1)

--- hw4/index2.js
+++ hw4/index2.js
@@ -11,33 +11,56 @@ const answerSqrt = ('sqrt');
 const answerSin = ('sin');
 const answerCos = ('cos');
 
-if(question === answerAdd || question === answerDiff || question === answerMult || question === answerDiv) {
-  const firstNum = parseInt(prompt('Enter first number', 0));
-  const secondNum = parseInt(prompt('Enter second number', 0));
+const sum = firstNum + secondNum;
+const diff = firstNum - secondNum;
+const mult = firstNum * secondNum;
+const div = firstNum / secondNum;
 
-  if(!firstNum || !secondNum) {
-    alert('This is bad digit, good by!');
+const resultAdd = `Calculation is finished!\n\nSum: ${firstNum} + ${secondNum} = ${sum}.`;
+const resultDiff = `Calculation is finished!\n\nSubtraction: ${firstNum} - ${secondNum} = ${diff}.`;
+const resultMult = `Calculation is finished!\n\nMultiplication: ${firstNum} * ${secondNum} = ${mult}.`;
+const resultDiv = `Calculation is finished!\n\nDivision: ${firstNum} / ${secondNum} = ${div}.`;
+
+if(question === answerSqrt || question === answerSin || question === answerCos) {
+  const num = parseInt(prompt('Enter number, 0'));
+
+  // if(num === null || undefined) {
+  //   alert('This is bad digit, good by!')
+  // }
+
+  // const resultRoot = `Calculation is finished!\n\nRoot: ${Math.sqrt(num)} = √${num}.`;
+  // const resultSin = `Calculation is finished!\n\nSin: ${Math.sin(num)} = sin(${num}).`;
+  // const resultCos = `Calculation is finished!\n\nCos: ${Math.cos(num)} = cos(${num}).`;
+
+  if(question === answerSqrt) {
+  alert(`Calculation is finished!\n\nRoot: ${Math.sqrt(num)} = √${num}.`);
   }
 
-  const sum = firstNum + secondNum;
-  const diff = firstNum - secondNum;
-  const mult = firstNum * secondNum;
-  const div = firstNum / secondNum;
+  if(question === answerSin) {
+    alert(`Calculation is finished!\n\nSin: ${Math.sin(num)} = sin(${num}).`);
+  }
 
-  const resultAdd = `Calculation is finished!\n\nSum: ${firstNum} + ${secondNum} = ${sum}.`;
-  const resultDiff = `Calculation is finished!\n\nSubtraction: ${firstNum} - ${secondNum} = ${diff}.`;
-  const resultMult = `Calculation is finished!\n\nMultiplication: ${firstNum} * ${secondNum} = ${mult}.`;
-  const resultDiv = `Calculation is finished!\n\nDivision: ${firstNum} / ${secondNum} = ${div}.`;
+  if(question === answerCos) {
+    alert(`Calculation is finished!\n\nCos: ${Math.cos(num)} = cos(${num}).`);
+  }
 
-  if(question === answerAdd) {
-    alert(resultAdd);
-  } else if(question === answerDiff) {
-    alert(resultDiff);
-  } else if(question === answerMult) {
-    alert(resultMult);
-  } else if(question === answerDiv) {
-    alert(resultDiv);
-  } 
-} else {
+} else if(question === answerAdd || question === answerDiff || question === answerMult || question === answerDiv) {
+    const firstNum = parseInt(prompt('Enter first number', 0));
+    const secondNum = parseInt(prompt('Enter second number', 0));
+  } else if(!firstNum || !secondNum) {
+      alert('This is bad digit, good by!');
+    }
+
+    if(question === answerAdd) {
+      alert(resultAdd);
+    } else if(question === answerDiff) {
+      alert(resultDiff);
+    } else if(question === answerMult) {
+      alert(resultMult);
+    } else if(question === answerDiv) {
+      alert(resultDiv);
+    } else {
   alert('Good by, see you later.')
 }
+
+
---
+++ hw4/index3.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const greetings = prompt('Welcome to calculator!');
+const question = prompt('What action you want to do? Add, Diff, Mult, Div, Sqrt, Sin, Cos');
+
+const answerAdd = ('add');
+const answerDiff = ('diff');
+const answerMult = ('mult');
+const answerDiv = ('div');
+const answerSqrt = ('sqrt');
+const answerSin = ('sin');
+const answerCos = ('cos');
+
+if(question === answerAdd ||question === answerDiff || question === answerDiv || question === answerMult) {
+  const firstNum = parseInt(prompt('Enter first number', 0));
+  const secondNum = parseInt(prompt('Enter second number', 0));
+
+  if(isNaN(firstNum) || isNaN(secondNum)) {
+    alert('This is bad digit, good by!');
+  }
+
+  const sum = firstNum + secondNum;
+  const diff = firstNum - secondNum;
+  const mult = firstNum * secondNum;
+  const div = firstNum / secondNum;
+
+  const resultAdd = `Calculation is finished!\n\nSum: ${firstNum} + ${secondNum} = ${sum}.`;
+  const resultDiff = `Calculation is finished!\n\nSubtraction: ${firstNum} - ${secondNum} = ${diff}.`;
+  const resultMult = `Calculation is finished!\n\nMultiplication: ${firstNum} * ${secondNum} = ${mult}.`;
+  const resultDiv = `Calculation is finished!\n\nDivision: ${firstNum} / ${secondNum} = ${div}.`;
+
+  if(question === answerAdd) {
+    alert(resultAdd);
+  } else if(question === answerDiff) {
+    alert(resultDiff);
+  } else if(question === answerMult) {
+    alert(resultMult);
+  } else if(question === answerDiv) {
+    if(secondNum === 0) {
+      alert('Division into zero is impossible.')
+    } else {
+      alert(resultDiv);
+    }
+  } 
+} else if(question === null || undefined) {
+  alert('Good by, see you later.')
+} else if(question === answerSqrt || question === answerSin || question === answerCos) {
+  const num = parseInt(prompt('Enter number, 0'));
+
+  if(isNaN(num)) {
+  alert('This is bad digit, good by!')
+} 
+
+  const resultRoot = `Calculation is finished!\n\nRoot: ${Math.sqrt(num)} = √${num}.`;
+  const resultSin = `Calculation is finished!\n\nSin: ${Math.sin(num)} = sin(${num}).`;
+  const resultCos = `Calculation is finished!\n\nCos: ${Math.cos(num)} = cos(${num}).`;
+
+  if(question === answerSqrt) {
+    alert(resultRoot);
+    } else if(question === answerSin) {
+      alert(resultSin);
+    } else if(question === answerCos) {
+      alert(resultCos);
+    }
+} else {
+  alert('Good by, see you later.')
+}
