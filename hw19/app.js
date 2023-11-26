e28f2b810d150a1465e3d95d6a55eaa43292ac09
TetianaYatchuk <tanya15115@gmail.com>
2023-11-26 16:53:03 +0200

    SuperMath

--- hw19/app.js
+++ hw19/app.js
@@ -1,22 +1,60 @@
 'use strict';
 
 class SuperMath {
-  check(obj) {
-    const x = obj;
-    const y = obj;
-    const znak = obj;
+  constructor() {
+    this.operations = ['+', '-', '*', '/', '%'];
+  }
 
-    if(x, y, znak) {
-      return prompt(`Do you want to do operation ${znak} with x=${x} and y=${y}?`);
+  check(obj) {
+    if(!obj) {
+      console.log('Invalid operator');
+      this.input();
+      return;
+    }
+
+    if (confirm(`Do you want to do this operation: x: ${obj.x}, y: ${obj.y}, znak: ${obj.znak} ?`)) {
+      const result = this.calculate(obj);
+      console.log(result);
     } else {
-      return this.input();
+      this.input();
     }
   }
 
   input() {
-    return prompt('Enter your number');
+    const x = parseFloat(prompt('Enter x'));
+    const y = parseFloat(prompt('Enter y'));
+    const znak = prompt('Enter znak');
+
+    if (!this.operations.includes(znak)) {
+      console.log('Invalid operator');
+      this.input();
+      return;
+    }
+
+    const obj = { x, y, znak };
+    this.check(obj);
+  }
+
+  calculate(obj) {
+    const { x, y, znak } = obj;
+
+    switch (znak) {
+        case '+':
+          return x + y;
+        case '-': 
+          return x - y;
+        case '*':
+           return x * y;
+        case '/':
+          return x / y;
+        case '%':
+          return x % y;
+        default:
+          console.log('Invalid operation');
+    }
   }
 }
 
 const myMath = new SuperMath();
-console.log(myMath);
+console.log(myMath.check());
+console.log(myMath.input());
