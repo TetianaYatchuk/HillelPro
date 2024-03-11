a6e4bba83343d74660bd70c828743a96a7f1f130
TetianaYatchuk <tanya15115@gmail.com>
2024-03-11 15:38:06 +0200

    fix code

--- hw10/utils/removeElement.js
+++ hw10/utils/removeElement.js
@@ -1,21 +1,34 @@
 'use strict';
 
-const firstUsersnumbers = prompt('Enter five any numbers whithout space, 12345').split('');
-const stringArray = prompt('Enter some string whithout space, asdfg').split('');
-const secondUsersnumber =  prompt('Enter three any numbers whithout space, 123').split('');
-const elementTodelete = prompt('Enter elements,which you want to delete, like: 1a2b7...');
+function arrayUnion(array1, array2) {
+  if (array1.length !== array2.length) return false
 
-const result = firstUsersnumbers.concat(stringArray, secondUsersnumber);
-
-console.log(result);
-
-const removeElement = (array, el) => {
-  let result = [];
-
-  for (const element of array) {
-    if (!el.includes(element)) result += element;
+  for (let i = 0; i < array1.length; i++) {
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      if (!arrayUnion(array1[i], array2[i])) {
+        return false
+      }
+    } else if (array1[i] !== array2[i]) {
+      return false
+    }
   }
-  return result;
+  return true
 }
 
-console.log(removeElement(result, elementTodelete).split(''));
+function removeEl(array, item) {
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(item) && Array.isArray(array[i])) {
+      if ( arrayUnion(item, array[i])) {
+        array.splice(i, 1)
+        i--
+      }
+    } else if (item === array[i]) {
+      array.splice(i, 1)
+      i--
+    }
+  }
+}
+
+const array = [1, 2, 3, 4, 5, 'string', [1, 2, 3]];
+removeEl(array, 'string');
+console.log(array)
